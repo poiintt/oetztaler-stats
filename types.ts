f8b2d04bcb0cc5c9524bab3f4414eca384b3569d
poiintt <abendmo@gmail.com>
2022-08-28 14:40:18 +0200

    feat: add SearchResult type

---
+++ types.ts
@@ -0,0 +1,36 @@
+export interface SearchResult {
+  dataCell: string[],
+  eventstatus: 'live',
+  marker: [
+    {
+      posbez: 'Letzte Zeit',
+      katrang: number,
+      meters: string,
+      color: string,
+      kmh: number,
+      bib: number,
+      time: string,
+      fore: string,
+      lat: number,
+      name: string,
+      long: number
+    }
+  ],
+  dataCellS: [],
+  track: number,
+  dt: number,
+  dtc: string,
+  infotext: string,
+  refnr: number,
+  data: [
+    {
+      datatyp: string,
+      color: string,
+      class: string,
+      aCells: (string | number)[]
+    }
+  ],
+  paging: boolean,
+  update: string,
+  statusinfo: 1
+}
