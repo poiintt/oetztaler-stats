b912edc92369e4d5614daea197634de59b51d5b0
poiintt <abendmo@gmail.com>
2022-08-28 14:39:45 +0200

    refactor: datasport.ts

--- datasport.ts
+++ datasport.ts
@@ -1,22 +1,21 @@
-import CryptoJS from 'crypto-js';
+import { enc, AES } from 'crypto-js'
 
 // from view-source:https://www.datasport.com/live/search/
-
-const key = CryptoJS.enc.Base64.parse('UHZ7Ivre3VQZz6OcueH7uw==');
-const iv = CryptoJS.enc.Base64.parse('ZPIU5yq6wJzBQXLDlH02/g==');
+const key = enc.Base64.parse('UHZ7Ivre3VQZz6OcueH7uw==')
+const iv = enc.Base64.parse('ZPIU5yq6wJzBQXLDlH02/g==')
 
 export function encrypt(payload: string): string {
-    console.time('encrypt')
-    const encrypted = CryptoJS.AES.encrypt(payload, key, { iv });
-    const result = encrypted.toString();
-    console.timeEnd('encrypt')
-    return result;
+  console.time('encrypt')
+  const encrypted = AES.encrypt(payload, key, { iv })
+  const result = encrypted.toString()
+  console.timeEnd('encrypt')
+  return result
 }
 
 export function decrypt(payload: string): string {
-    console.time('decrypt')
-    const decrypted = CryptoJS.AES.decrypt(payload, key, { iv });
-    const result = decrypted.toString(CryptoJS.enc.Utf8);
-    console.timeEnd('decrypt')
-    return result;
+  console.time('decrypt')
+  const decrypted = AES.decrypt(payload, key, { iv })
+  const result = decrypted.toString(enc.Utf8)
+  console.timeEnd('decrypt')
+  return result
 }
