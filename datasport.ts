c5e4dfafde7dcec47b815401ffe0952523df525c
poiintt <abendmo@gmail.com>
2022-08-28 13:47:22 +0200

    chore: add @types/crypto-js

--- datasport.ts
+++ datasport.ts
@@ -2,21 +2,21 @@ import CryptoJS from 'crypto-js';
 
 // from view-source:https://www.datasport.com/live/search/
 
-var key = CryptoJS.enc.Base64.parse('UHZ7Ivre3VQZz6OcueH7uw==');
-var iv = CryptoJS.enc.Base64.parse('ZPIU5yq6wJzBQXLDlH02/g==');
+const key = CryptoJS.enc.Base64.parse('UHZ7Ivre3VQZz6OcueH7uw==');
+const iv = CryptoJS.enc.Base64.parse('ZPIU5yq6wJzBQXLDlH02/g==');
 
 export function encrypt(payload: string): string {
     console.time('encrypt')
-    var encrypted = CryptoJS.AES.encrypt(payload, key, { iv: iv });
-    var result = encrypted.toString();
+    const encrypted = CryptoJS.AES.encrypt(payload, key, { iv });
+    const result = encrypted.toString();
     console.timeEnd('encrypt')
     return result;
 }
 
 export function decrypt(payload: string): string {
     console.time('decrypt')
-    var decrypted = CryptoJS.AES.decrypt(payload, key, { iv: iv });
-    var result = decrypted.toString(CryptoJS.enc.Utf8);
+    const decrypted = CryptoJS.AES.decrypt(payload, key, { iv });
+    const result = decrypted.toString(CryptoJS.enc.Utf8);
     console.timeEnd('decrypt')
     return result;
 }
